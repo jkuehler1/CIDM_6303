79e822f24c6bf39ef1b4a9627cccc9568bd0c9a9
jkuehler1 <112365315+jkuehler1@users.noreply.github.com>
2023-03-07 04:28:18 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,130 @@
-print("Syntax errors means there is a problem in the grammar of the expression.")
-print("." * 5)
-print("An IDE is a code editor with features for autocompletion and debugging.")
+# Chapter 2.1 and 2.2 Variables
+import math
+students_count = 1000
+rating = 4.99
+is_published = True
+course_name = "Python Programming"
+print(students_count)
+print(rating, is_published, course_name)
+print("."*10)
+
+# Chapter 2.3 Strings
+course = "Python Programming"
+print(len(course))
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[0:])
+print(course[:3])
+print(course[:])
 print("." * 10)
-print("File extensions for a python app is .py")
-print("." * 15)
-print("A linter is to check for syntax errors.")
-print("." * 20)
-print("PEP8 is a style guide for best practices in Python")
-print("." * 25)
-print("CPython is the default implementation of Python. ")
-print("." * 30)
-print("Machine code is specific to a type of operating system/CPU.")
-print("." * 30)
-print("Order of execution: Python language, cPython, bytecode, virtual machine, machine code")
-print("." * 30)
+
+# Chapter 2.4 Escape Sequences
+# \"
+course = "Python \"Programming"
+print(course)
+# \'
+course = "Python \'Programming"
+print(course)
+# \\
+course = "Python \\Programming"
+print(course)
+print("." * 10)
+
+# Chapter 2.5 Formatted Strings
+first = "Jeremy"
+last = "Kuehler"
+full = f"{len (first)} {2+ 2}"
+print(full)
+print("." * 10)
+
+# Chapter 2.6 String Methods
+print("Chapter 2.6 String Methods")
+course = " python programming"
+print(course.upper())
+print(course. lower())
+print(course.title())
+print(course.rstrip())
+print(course.find("Pro"))
+print(course.replace("p", "j"))
+print("pro" in course)
+print("swift" not in course)
+print("." * 10)
+
+# Chapter 2.7 Numbers"
+print("Chapter 2.7 Numbers")
+x = 1  # integer
+x = 1.1  # float number with decimals
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10/3)
+print(10 // 3)
+print(10 % 3)  # modulous or mod
+print(10 ** 3)  # exponent
+
+x = 10
+x = x + 3
+print(x)
+x = 10
+x += 3  # augmented operator add
+print(x)
+
+y = 20
+y -= 3  # augmented operator subtract
+print(y)
+
+z = 30
+z *= 3  # augmented operator multiply
+print(z)
+
+
+# Chapter 2.8 Working with Numbers
+print("Chapter 2.8 Working with Numbers")
+
+print(round(2.9))
+print(abs(-2.0))
+print(math.ceil(2.2))
+print("."*10)
+
+# Chapter 2.9 Type Conversion
+input("Enter a value for x: ")
+y = int(x) + 1
+print(f"x: {x}, y: {y}")
+print("." * 10)
+
+rate = input("Enter interest rate, e.g. 0.5: ")
+rate = float(rate)  # may resuse the same variable
+# CRITICAL KNOWLEDGE
+# three different ways to output a number variable with text
+print(f"Borrower does not qualify at {rate}")  # string interpolation
+print("Borrower does not qualify at ", rate)
+print("Borrow does not qualify at " + str(rate))  # convert to string
+print()
+# Dr. Humpherys likes string interpolation the best!
+
+# Displaying decimals in Strings
+grams = 15.125
+print(f"Weight is {grams}")  # no formatting
+print(f"Weight is {grams:.2f}")  # two decimals, f means float
+print(f"Weight is {grams:.4f}")  # four decimals
+print(f"Weight is {grams:.0f}")  # zero decimals
+
+
+# Task G
+
+card_number = "xxx8974"
+date = "9\\7\\2020"
+cookies_cost = 3.15
+chips_cost = 4.58
+salsa_cost = 5.10
+total_cost = chips_cost + salsa_cost + cookies_cost
+
+print("*"*15)
+print("Receipt")
+print(f"Date: {date}")
+print(f"cookies $ {cookies_cost:.2f}")
+print(f"chips $ {chips_cost:.2f}")
+print(f"salsa $ {salsa_cost:.2f}")
+print(f"Total: $ {total_cost:.2f}")
+print("*"*15)
